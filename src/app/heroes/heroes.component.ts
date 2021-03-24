818acec7564635b33a7aa26c982605a10c82eea8
Luis Felipe Cadavid <felipe.cadavid@udea.edu.co>
2021-03-23 19:20:07 -0500

    Revert "Se termina parte 4 de tutorial, con la implementación de mensajes adicionales en heroes.component.ts"
    This reverts commit 720b4df178887b079fd99c7c29f7444cb8bea728.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
-
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -18,7 +16,7 @@ export class HeroesComponent implements OnInit {
 
 
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
@@ -26,9 +24,9 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
-  }
+  this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+}
 
   ngOnInit() {
     this.getHeroes();
